310a701d957be4232729cf6910d9fd692c222ada
Vinh Nguyen <vinh.nglx@gmail.com>
2016-02-09 09:37:26 +0700

    Rename haphaisos to gaea on specs

--- spec/gaea_spec.rb
+++ spec/gaea_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe Hephaistos do
+describe Gaea do
   it 'has a version number' do
-    expect(Hephaistos::VERSION).not_to be nil
+    expect(Gaea::VERSION).not_to be nil
   end
 
   it 'does something useful' do
