e6827770d8cc5a248904f7180641f14fc3fcf650
Vinh Nguyen <vinh.nglx@gmail.com>
2016-02-09 22:44:01 +0700

    Implement search on RubyGem

--- spec/support/mocking.rb
+++ spec/support/mocking.rb
@@ -9,3 +9,15 @@ shared_context 'questions list' do
       to_return(status: 400, body: File.open(File.dirname(__FILE__) + '/fixtures/' + 'bad_request.json', 'rb').read, headers: {})
   end
 end
+
+shared_context 'gems list' do
+  before do
+    stub_request(:get, "http://rubygems.org/api/v1/search.json?query=weer").
+      with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+      to_return(status: 200, body: File.open(File.dirname(__FILE__) + '/fixtures/' + 'gems.json', 'rb').read, headers: {})
+
+    stub_request(:get, "http://rubygems.org/api/v1/search.json?query=inv4lid").
+      with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+      to_return(status: 200, body: File.open(File.dirname(__FILE__) + '/fixtures/' + 'nil_gem.json', 'rb').read, headers: {})
+  end
+end
