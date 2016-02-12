ab926cc95d5c75ad266e5b57fea15a01127d70d6
Vinh Nguyen <vinh.nglx@gmail.com>
2016-02-12 21:43:28 +0700

    Implement Confreaks

---
+++ spec/lib/confreaks_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+RSpec.describe Confreaks do
+  include_context 'confreaks list'
+  let(:conf) { Confreaks.new("2015") }
+
+  context '.initialize' do
+    it 'returns an instance of Confreaks object' do
+      expect(conf).to be_an_instance_of Confreaks
+    end
+
+    it 'returns constructor of Confreaks object' do
+      expect(conf.year).to eq "2015"
+    end
+  end
+
+  context '.event_of_year' do
+    describe 'with a field - for example: ruby, js or python' do
+      it 'returns a list of conferences of a field in year - for example: ruby' do
+        expect(conf.events_of_year("ruby").rows.count).to eq 2
+      end
+
+      it 'creates terminal table object' do
+        expect(conf.events_of_year("ruby")).to be_an_instance_of Terminal::Table
+      end
+    end
+
+    describe 'without a field' do
+      it 'return a list of conferences in year' do
+        expect(conf.events_of_year.rows.count).to eq 4
+      end
+
+      it 'creates terminal table object' do
+        expect(conf.events_of_year).to be_an_instance_of Terminal::Table
+      end
+    end
+  end
+end
